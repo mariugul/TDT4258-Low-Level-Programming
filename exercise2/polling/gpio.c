a6c5b7e42ac0d11e1006f068f8fbe1631f1bbf52
mariugul <marius_gulbrandsen@hotmail.no>
2019-10-09 14:15:48 +0200

    updated gpio.c

--- exercise2/polling/gpio.c
+++ exercise2/polling/gpio.c
@@ -3,20 +3,78 @@
 #include "efm32gg.h"
 #include "gpio.h"
 
+// LED VALUES
+#define LEDS_OFF 		0xFF00
+#define LEDS_ON  		0x00FF
+
+// DRIVE STRENGTHS
+#define STANDARD_DRIVE	0x00
+#define LOWEST_DRIVE	0x01
+#define LOW_DRIVE		0x03
+#define HIGH_DRIVE		0x02
+
 /*
  * function to set up GPIO mode and interrupts
  */
 void gpio_init()
 {
-  *CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_GPIO;	// enable GPIO clock
-  *GPIO_PA_CTRL = 2;		// set high drive strength 
-  *GPIO_PA_MODEH = 0x55555555;	//set pins A8-15 as output 
-  *GPIO_PC_MODEL = 0x33333333; // Set pins C0-7 as input 
-  *GPIO_PC_DOUT = 0xFF; /* Enable internal pull-up*/
-  *GPIO_EXTIPSELL = 0x22222222;
-  *GPIO_EXTIFALL = 0xFF; /* Interrupt on falling edge */
-    //*GPIO_EXTIRISE = 0xFF; /* Interrupt on rising edge */
-  *GPIO_IEN = 0x00FF;
-  *GPIO_IFC = 0xFF;
-  *GPIO_PA_DOUT = 0xFF00;
+	gpio_enable_clock();
+  	gpio_set_drive();
+  	gpio_set_pins();
+  	gpio_enable_interrupt();
+	gpio_leds_off();
 }
+
+void gpio_leds_on()
+{
+	*GPIO_PA_DOUT = LEDS_ON;					// Turns leds on
+}
+
+void gpio_leds_off()
+{
+	*GPIO_PA_DOUT = LEDS_OFF;					// Turns leds off
+}
+
+void gpio_enable_clock()
+{
+	*CMU_HFPERCLKEN0 |= CMU2_HFPERCLKEN0_GPIO;	// Enable GPIO clock
+}
+
+void gpio_set_drive()
+{
+	*GPIO_PA_CTRL   = 2;			// Set high drive strength 
+}
+
+void gpio_set_pins()
+{
+	*GPIO_PA_MODEH  = 0x55555555;	// Set pins A8-15 as output 
+  	*GPIO_PC_MODEL  = 0x33333333;   // Set pins C0-7 as input 
+  	*GPIO_PC_DOUT   = 0xFF; 		// Enable internal pull-up
+}
+
+
+/* Maybe move this to interrupt.c? 
+   It uses the GPIO register so it
+   might make sense to keep it here */
+void gpio_enable_interrupt()
+{
+	*GPIO_EXTIPSELL = 0x22222222;
+  	*GPIO_EXTIFALL  = 0xFF;  		// Interrupt on falling edge 
+    //*GPIO_EXTIRISE = 0xFF; 		// Interrupt on rising edge 
+    *GPIO_IEN       = 0x00FF;		// Does what?
+    *GPIO_IFC       = 0xFF;			// Does what? Set interrupt flag?
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
